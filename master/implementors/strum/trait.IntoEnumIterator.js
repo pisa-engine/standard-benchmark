a2f3d3b9fa02765076ebef05e06d50bd2510e398
Travis CI User <travis@example.org>
2019-06-03 01:42:00 +0000

    Automatic Travis documentation build

---
+++ master/implementors/strum/trait.IntoEnumIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["stdbench"] = [{text:"impl <a class=\"trait\" href=\"strum/trait.IntoEnumIterator.html\" title=\"trait strum::IntoEnumIterator\">IntoEnumIterator</a> for <a class=\"enum\" href=\"stdbench/run/enum.TrecTopicField.html\" title=\"enum stdbench::run::TrecTopicField\">TrecTopicField</a>",synthetic:false,types:["stdbench::run::TrecTopicField"]},{text:"impl <a class=\"trait\" href=\"strum/trait.IntoEnumIterator.html\" title=\"trait strum::IntoEnumIterator\">IntoEnumIterator</a> for <a class=\"enum\" href=\"stdbench/enum.Stage.html\" title=\"enum stdbench::Stage\">Stage</a>",synthetic:false,types:["stdbench::Stage"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ master/stdbench/command/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["ExtCommand","Extension of `Command`."]]});
